408aa90e06a68361771f406b497a4702e63cbaf7
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-04-06 23:24:55 +0900

    Shareに対応

--- Data/Master/MonsterMB.cs
+++
@@ -1,5 +0,0 @@
-public class MonsterMB {
-    public long id { get; set; }
-    public string name { get; set; }
-    public int initialGrade { get; set; }
-}
--- Data/User/UserMonsterInfo.cs
+++
@@ -1,7 +0,0 @@
-public class UserMonsterInfo{
-    public long id { get;set; }
-    public long monsterId {get; set; }
-    public string playFabId { get; set; }
-    public int level { get; set; }
-    public int grade { get; set; }
-}
